28ec28533bc6e5b0d484aa3c745d4fbece0b5c1a
Natanael <natanael_magalhaes@msn.com>
2019-10-25 01:46:16 -0300

    convert image to base64 and print the string

---
+++ includes/img-to-base64.php
@@ -0,0 +1,9 @@
+<?php
+
+function img_to_base64($path) {
+  $type = pathinfo($path, PATHINFO_EXTENSION);
+  $data = file_get_contents($path);
+  $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+  
+  echo $base64;
+}
