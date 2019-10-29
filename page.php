5fe58ca55a21b20694f851dcce9dafcc3020c1f4
Natanael <natanael_magalhaes@msn.com>
2019-10-29 01:27:27 -0300

    show message for users trying to load private page

--- page.php
+++ page.php
@@ -5,7 +5,13 @@ get_template_part('partials/navbar');
 
 ?>
 
-<?php $p = get_post(); ?>
+<?php 
+
+$p = get_post();
+$post_meta = get_post_meta($p->ID);
+$visibility = $post_meta['_meta_is_private'][0];
+
+?>
 
 <section class="page-hero">
   <div class="container">
@@ -20,7 +26,19 @@ get_template_part('partials/submenus');
 <main class="page-content">
   <section class="mt-5 pt-5">
     <div class="container">
-      <?php echo $p->post_content; ?>
+      <?php if (!is_user_logged_in() &&  $visibility === 'private'): ?>
+      <div class="jumbotron">
+        <h1 class="display-4">Private Content</h1>
+        <p class="lead">This content is reserved for authenticated users.</p>
+        <hr class="my-4">
+        <p><a href="/register">Register</a> a new account or <a href="/login">Login</a> to your account</p>
+        <a class="btn base btn-lg mr-3" href="/register" role="button">Register</a>
+        <a class="btn base btn-lg" href="/login" role="button">Login</a>
+      </div>
+      <div class="pb-5"></div>
+      <?php else:
+      echo $p->post_content;
+      endif; ?>
     </div>
   </section>
 </main>
