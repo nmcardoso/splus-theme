eb1abb2fb4feb838bfe4a49440fd31463262bbe1
Natanael <natanael_magalhaes@msn.com>
2020-02-06 18:43:35 -0300

    update bootstrap and fonts html references to local files

--- header.php
+++ header.php
@@ -7,12 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,800&display=swap" rel="stylesheet">
-  <link href="//db.onlinewebfonts.com/c/dd7e0fcaaf9a62550b7bcfa1b4f0dcd9?family=Plantagenet+Cherokee" rel="stylesheet"
-    type="text/css" />
-  <link href="https://fonts.googleapis.com/css?family=Raleway:400,600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/style/bootstrap-4.3.1.min.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/style/fonts.css">
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/mosaic.css">
 
