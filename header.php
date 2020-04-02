f2d2654db0a248dec6295d2b38b04552082a30ba
Natanael <natanael_magalhaes@msn.com>
2020-04-01 23:19:48 -0300

    :art: favicon

--- header.php
+++ header.php
@@ -6,6 +6,17 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+  <!-- Favicon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_url'); ?>/img/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_url'); ?>/img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_url'); ?>/img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="<?php bloginfo('template_url'); ?>/img/favicon/site.webmanifest">
+  <link rel="mask-icon" href="<?php bloginfo('template_url'); ?>/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+  <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/img/favicon/favicon.ico">
+  <meta name="msapplication-TileColor" content="#2d89ef">
+  <meta name="msapplication-config" content="<?php bloginfo('template_url'); ?>/img/favicon/browserconfig.xml">
+  <meta name="theme-color" content="#ffffff">
+
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/style/bootstrap-4.3.1.min.css">
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/style/fonts.css">
