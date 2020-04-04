e6a2ed0531612a73d824411ab5e468161d50094c
Natanael <natanael_magalhaes@msn.com>
2020-04-04 13:43:15 -0300

    remove Version field of style.css

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -1,10 +1,5 @@
 BUILD_NAME=build+`git rev-parse --short HEAD`
 
-# Update version number
-sed -i "2iVersion: $BUILD_NAME" style.css
-git add .
-git commit -m "update version"
-
 # Create .zip file understandable by wordpress
 mkdir zipball
 git archive --prefix=splus-theme/ -o zipball/zipball.zip HEAD
