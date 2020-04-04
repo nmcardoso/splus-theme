93b58acf30b850253182a34c52ea1c229eec91ce
Natanael <natanael_magalhaes@msn.com>
2020-04-04 12:39:51 -0300

    update build file to change version number (#10)

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -1,4 +1,11 @@
+BUILD_NAME=build+`git rev-parse --short HEAD`
+
+# Update version number
+sed -i "2iVersion: $BUILD_NAME" style.css
+git add .
+git commit -m "update version"
+
+# Create .zip file understandable by wordpress
 mkdir zipball
 git archive --prefix=splus-theme/ -o zipball/zipball.zip HEAD
-echo 'build+'`git rev-parse --short HEAD` > zipball/version.txt
-
+echo $BUILD_NAME > zipball/version.txt
