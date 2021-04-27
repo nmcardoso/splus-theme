ba692ed6b518c7c90f0320fe132d6b9af722faa6
Natanael <natanael_magalhaes@msn.com>
2021-04-27 20:54:37 -0300

    Revert "hardcode website url, fix https issue."
    This reverts commit 0f1406115e111b7b71156991a909d5e43f4ef663.

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="<?php echo "https://www.splus.iag.usp.br"; ?>/assets/script/jquery-3.3.1.slim.min.js"></script>
-  <script src="<?php echo "https://www.splus.iag.usp.br"; ?>/assets/script/popper-1.14.7.min.js"></script>
-  <script src="<?php echo "https://www.splus.iag.usp.br"; ?>/assets/script/bootstrap-4.3.1.min.js"></script>
-  <script src="<?php echo "https://www.splus.iag.usp.br"; ?>/script.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/assets/script/jquery-3.3.1.slim.min.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/assets/script/popper-1.14.7.min.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/assets/script/bootstrap-4.3.1.min.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/script.js"></script>
 </body>
 
 </html>
--- header.php
+++ header.php
@@ -7,21 +7,21 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Favicon -->
-  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo "https://www.splus.iag.usp.br"; ?>/img/favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo "https://www.splus.iag.usp.br"; ?>/img/favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo "https://www.splus.iag.usp.br"; ?>/img/favicon/favicon-16x16.png">
-  <link rel="manifest" href="<?php echo "https://www.splus.iag.usp.br"; ?>/img/favicon/site.webmanifest">
-  <link rel="mask-icon" href="<?php echo "https://www.splus.iag.usp.br"; ?>/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
-  <link rel="shortcut icon" href="<?php echo "https://www.splus.iag.usp.br"; ?>/img/favicon/favicon.ico">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_url'); ?>/img/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_url'); ?>/img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_url'); ?>/img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="<?php bloginfo('template_url'); ?>/img/favicon/site.webmanifest">
+  <link rel="mask-icon" href="<?php bloginfo('template_url'); ?>/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+  <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/img/favicon/favicon.ico">
   <meta name="msapplication-TileColor" content="#2d89ef">
-  <meta name="msapplication-config" content="<?php echo "https://www.splus.iag.usp.br"; ?>/img/favicon/browserconfig.xml">
+  <meta name="msapplication-config" content="<?php bloginfo('template_url'); ?>/img/favicon/browserconfig.xml">
   <meta name="theme-color" content="#ffffff">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="<?php echo "https://www.splus.iag.usp.br"; ?>/assets/style/bootstrap-4.3.1.min.css">
-  <link rel="stylesheet" href="<?php echo "https://www.splus.iag.usp.br"; ?>/assets/style/fonts.css">
-  <link rel="stylesheet" href="<?php echo "https://www.splus.iag.usp.br"; ?>/style.css">
-  <link rel="stylesheet" href="<?php echo "https://www.splus.iag.usp.br"; ?>/mosaic.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/style/bootstrap-4.3.1.min.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/style/fonts.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/mosaic.css">
 
   <link href="https://fonts.googleapis.com/css2?family=Jost:wght@700&display=swap" rel="stylesheet">
 
