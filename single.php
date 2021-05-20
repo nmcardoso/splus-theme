fe237e16a729e2738ae60c693c0805514ac3c4a7
Natanael <nmcardoso@users.noreply.github.com>
2021-05-20 12:59:56 -0300

    implementation of single.php based on page.php

--- single.php
+++ single.php
@@ -1,97 +1,50 @@
-<?php get_header(); ?>
+<?php 
+add_filter('body_class', function() { return ['d-flex', 'flex-column']; });
+get_header();
+?>
 
-  <section class="page-hero">
+<div style="flex: 1 0 auto;"> <!-- make footer sticky bottom -->
+
+<?php
+get_template_part('partials/navbar');
+
+$p = get_post();
+$post_meta = get_post_meta($p->ID);
+$visibility = $post_meta['_meta_is_private'][0];
+?>
+
+<section class="page-hero">
+  <div class="container">
+    <h1><?php echo $p->post_title; ?></h1>
+  </div>
+</section>
+
+<?php
+  get_template_part('partials/submenus');
+?>
+
+<main class="page-content">
+  <section class="mt-5">
     <div class="container">
-      <h1>DESCRIPTION OF THE S-PLUS SURVEYS</h1>
+      <?php if (!is_user_logged_in() &&  $visibility === 'private'): ?>
+      <div class="jumbotron">
+        <h1 class="display-4 mt-0">Private Content</h1>
+        <p class="lead">This content is reserved for authenticated users.</p>
+        <hr class="my-4">
+        <p><a href="/register">Register</a> a new account or <a href="/login">Login</a> to your account</p>
+        <a class="btn base btn-lg mr-3" href="/register" role="button">Register</a>
+        <a class="btn base btn-lg" href="/login" role="button">Login</a>
+      </div>
+      <div class="pb-5"></div>
+      <?php else:
+      echo do_shortcode($p->post_content);
+      endif; ?>
     </div>
   </section>
+</main>
 
-  <main class="page-content">
-    <section class="mt-5 pt-5">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-8">
-            <img src="img/photo3.png" width="100%" alt="">
-          </div>
-        </div>
-      </div>
-    </section>
+</div> <!-- footer sticky -->
 
-    <section class="mt-5">
-      <div class="container">
-        <div class="row">
-          <div class="col-6">
-            <h1>Main Survey</h1>
-            <p class="text-justify">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Blandit turpis cursus in hac habitasse platea dictumst quisque.
-              Proin sagittis nisl rhoncus mattis rhoncus urna neque.
-              Imperdiet massa tincidunt nunc pulvinar. Mauris rhoncus aenean
-              vel elit scelerisque mauris pellentesque. Quisque non tellus
-              orci ac. Ac feugiat sed lectus vestibulum. Pharetra massa massa
-              ultricies mi quis hendrerit. Pulvinar sapien et ligula ullamcorper.
-              Praesent elementum facilisis leo vel fringilla est ullamcorper.
-              Turpis egestas integer eget aliquet nibh praesent. Congue quisque
-              egestas diam in arcu. Felis imperdiet proin fermentum leo. Fringilla
-              est ullamcorper eget nulla facilisi etiam dignissim diam quis.
-              Amet justo donec enim diam vulputate. Dictum varius duis at
-              consectetur lorem. Sed faucibus turpis in eu mi bibendum neque.
-              Malesuada nunc vel risus commodo viverra. Pulvinar elementum
-              integer enim neque. Vulputate eu scelerisque felis imperdiet.
-              Accumsan in nisl nisi scelerisque eu ultrices vitae. Morbi quis
-              commodo odio aenean sed adipiscing diam donec adipiscing. Duis at
-              tellus at urna condimentum mattis. Arcu bibendum at varius vel
-              pharetra vel. Rutrum quisque non tellus orci ac. Lectus arcu
-              bibendum at varius vel pharetra vel turpis. Commodo ullamcorper
-              a lacus vestibulum sed. Ultricies tristique nulla aliquet enim
-              tortor at. Neque sodales ut etiam sit amet nisl purus in. Nec
-              ullamcorper sit amet risus nullam eget felis. Pellentesque adipiscing
-              commodo elit at. Donec pretium vulputate sapien nec sagittis
-              aliquam malesuada. Enim ut sem viverra aliquet eget sit amet t
-              ellus cras. Sed enim ut sem viverra aliquet eget sit amet. Aen
-              ean euismod elementum nisi quis eleifend quam adipiscing vitae
-              . Sollicitudin aliquam ultrices sagittis orci a scelerisque pur
-              us.
-            </p>
-          </div>
-
-          <div class="col-6">
-            <img src="img/photo4.png" alt="" width="100%">
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section class="mt-5">
-      <div class="container">
-        <div class="row">
-          <div class="col-6">
-            <h1>SHORT SURVEY</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Blandit turpis cursus in hac habitasse platea dictumst quisque. Proin sagittis nisl
-              rhoncus mattis rhoncus urna neque. Imperdiet massa tincidunt nunc pulvinar. Mauris rhoncus aenean vel elit
-              scelerisque mauris pellentesque. Quisque non tellus orci ac. Ac feugiat sed lectus vestibulum. Pharetra
-              massa massa ultricies mi quis hendrerit. Pulvinar sapien et ligula ullamcorper. Praesent elementum
-              facilisis leo vel fringilla est ullamcorper. Turpis egestas integer eget aliquet nibh praesent. Congue
-              quisque egestas diam in arcu.
-            </p>
-          </div>
-          <div class="col-6">
-            <h1>GALACTIC SURVEY</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Blandit turpis cursus in hac habitasse platea dictumst quisque. Proin sagittis nisl
-              rhoncus mattis rhoncus urna neque. Imperdiet massa tincidunt nunc pulvinar. Mauris rhoncus aenean vel elit
-              scelerisque mauris pellentesque. Quisque non tellus orci ac. Ac feugiat sed lectus vestibulum. Pharetra
-              massa massa ultricies mi quis hendrerit. Pulvinar sapien et ligula ullamcorper. Praesent elementum
-              facilisis leo vel fringilla est ullamcorper.
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
-  </main>
+<?php get_template_part('partials/footer'); ?>
 
 <?php get_footer(); ?>
